1f8832f15c2e2b10f310a8ade4aef51e1b8b3852
Areunormal <taurus0503@mail.ru>
2022-12-04 17:01:22 +0300

    Dz№3 dop

---
+++ dz3_4.py
@@ -0,0 +1,28 @@
+import math as m
+i =  0
+while (i != 1):
+    try:
+        a,b,c=map(float,input("Введи коэффициенты a,b,c в твоем уравнении(через пробел): ").split())
+    except ValueError:
+        print("Error, коэффициенты должны быть числами")
+    else:
+        i = 1
+discr = (b**2-(4*a*c))
+print("Дискриминант равен = ",discr)
+while (i!=0):
+    try:
+        koren = m.sqrt(discr)
+    except ValueError:
+        print("Корней нет")
+        break
+    x1 = float((-b + koren)/(2*a))
+    x2 = float((-b - koren)/(2*a))
+    if (discr > 0):
+        print("x1 = ",x1)
+        print("x2 = ",x2)
+        i = 0
+    elif (discr == 0):
+        print("x = ",x1)
+        i = 0
+    else:
+        None
