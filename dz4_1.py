6c1854a8412d4e4c3adcef768ca19457d65f75ac
Areunormal <taurus0503@mail.ru>
2022-12-08 17:01:40 +0300

    Dz4

---
+++ dz4_1.py
@@ -0,0 +1,23 @@
+chisla=[]
+while True:
+    dobavit  = str(input("Dobavit' chislo?(+/-): "))
+    if dobavit == "-":
+        break
+    elif dobavit =="+":
+        try:
+            chislo = float(input("Vvedi chislo dlya massiva: "))
+            chisla.append(chislo)
+        except ValueError:
+            print("Error, vvedi chislo: ")
+    else:
+        print("Error, vvedi + ili -")
+for i in range(len(chisla)-1):
+    for j in range  (len(chisla)-i-1):
+        if chisla[j] > chisla [j+1]:
+            a = chisla[j]
+            b = chisla[j+1]
+            chisla[j] = b
+            chisla[j+1] = a
+        else:
+            None  
+print(chisla)
---
+++ dz4_2.py
@@ -0,0 +1,2 @@
+slovar = {"red":(255,0,0),"yellow":(255,255,0),"green":(0,255,0),"blue":(0,0,255),"black":(0,0,0)}
+print(slovar.get('red'))
