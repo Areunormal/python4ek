558458b1968a8f5e2def93cfc81213774b16d73a
Areunormal <taurus0503@mail.ru>
2022-12-22 15:14:56 +0300

    pravki

--- dz4_3.py
+++ dz4_3.py
@@ -1,13 +1,10 @@
-"""Сравнение двух произвольных наборов чисел"""
 import random
 nabor1 =set()
 nabor2 =set()
-"""Задаются произвольные наборы"""
 dlina=random.randint(1,20)
 for i in range (dlina):
     nabor1.add(random.randint(1,100))
     nabor2.add(random.randint(1,100))
-"""Вывод сравнений"""
 print("2 randomnix nabora:", nabor1, nabor2)
 print ("peresechenie naborov:",nabor1.intersection(nabor2))
 print ("est v pervom nabore, no net vo vtorov:",nabor1.difference(nabor2))
